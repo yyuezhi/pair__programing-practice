388f90bbd72a1835bb3f479b1e59b56172cc102b
Yz <wddyzq@gmail.com>
2019-10-29 21:13:22 +0800

    compute by index

--- colab/ascii.py
+++ colab/ascii.py
@@ -1,13 +1,14 @@
 def computeSimple(string):
     sum = 0
     # start writing here
-    
+
 
     return sum
 
 def computeByIndex(string):
     sum = 0
-    # start writing here
+    for i in range(len(string)):
+        sum+=(ord(string[i])*i)
 
 
     return sum
