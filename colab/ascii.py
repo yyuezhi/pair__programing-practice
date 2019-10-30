47a80519799f9d485a4acf9e0328befec525256a
yyuezhi <48882898+yyuezhi@users.noreply.github.com>
2019-10-30 16:10:55 +0800

    asd

--- colab/ascii.py
+++ colab/ascii.py
@@ -2,14 +2,19 @@ def computeSimple(string):
     sum = 0
     for i in string:
         sum+=ord(i)
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> a2321f03c33253929d743c3b0316af6efdd7b57b
 
     return sum
 
 def computeByIndex(string):
     sum = 0
     # start writing here
-
+    for i in range(len(string)):
+        sum+=(i*ord(string[i]))
 
     return sum
 
